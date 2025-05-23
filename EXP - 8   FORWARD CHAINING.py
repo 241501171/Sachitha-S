4b5249e05caf89ded9757330c7b14f1aefd11fb1
241501171 <sachitha.s.2024.aiml@rajalakshmi.edu.in>
2025-05-23 21:45:19 +0530

    Update EXP - 8   FORWARD CHAINING.py

--- EXP - 8   FORWARD CHAINING.py
+++ EXP - 8   FORWARD CHAINING.py
@@ -10,18 +10,25 @@ rules = [
     ('f', ['d', 'e'])
 ]
 
-def backward_chaining(goal, facts, rules):
-    if goal in facts:
-        return facts[goal]
-    for head, body in rules:
-        if head == goal:
-            if all(backward_chaining(fact, facts, rules) for fact in body):
-                return True
-    return False
+def forward_chaining(facts, rules, goal):
+    inferred = set(facts.keys())
+    new_inferred = set(facts.keys())
+    while new_inferred:
+        current_inferred = set()
+        for rule in rules:
+            head, body = rule
+            if head not in inferred and all(fact in inferred for fact in body):
+                current_inferred.add(head)
+        if current_inferred:
+            inferred.update(current_inferred)
+            new_inferred = current_inferred
+        else:
+            new_inferred = set()
+    return goal in inferred
 
 goal = 'f'
 
-if backward_chaining(goal, facts, rules):
+if forward_chaining(facts, rules, goal):
     print(f"The goal '{goal}' can be achieved.")
 else:
     print(f"The goal '{goal}' cannot be achieved.")
